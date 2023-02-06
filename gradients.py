ce405bc6967932ca914864e82a3dff1170723e13
dcacciarelli <dcacciarelli>
2023-02-06 15:53:59 +0100

    added files

--- gradients.py
+++ gradients.py
@@ -28,15 +28,79 @@ def get_integrated_gradients(model, inp, baseline, steps=20):
     # 2. Compute gradients
     grads = []
     for elem in path_inputs:
-        # print(elem)
         grad = get_grads(torch.Tensor(elem[0]), model)
-        grads.append(grad[0])
-    grads = torch.from_numpy(np.array(grads))
+        grads.append(np.array(grad))
 
     # 3. Approximate the integral using the trapezoidal rule
-    grads = (grads[:-1] + grads[1:]) / 2.0
-    avg_grads = torch.mean(grads, axis=0)
+    delta = 1 / steps
+    integrated_grads = delta * np.sum((inp - baseline) * grads, axis=0)
+
+    return integrated_grads, np.mean(grads, axis=0)
+
+
+def get_integrated_gradients_mc(model, inp, baseline, samples=1000):
+
+    # If baseline is not provided, start with an array of zeros
+    if baseline is None:
+        baseline = np.zeros(len(inp)).astype(np.float32)
+    else:
+        baseline = baseline
+
+    baseline = np.array(baseline).reshape(-1, len(inp))
+    inp = np.array(inp).reshape(-1, len(inp))
+
+    # Generate random samples
+    random_samples = np.random.uniform(0, 1, (samples, len(inp)))
+    random_vectors = baseline + (inp - baseline) * random_samples
+
+    # Compute gradients for each random vector
+    grads = []
+    for i in range(samples):
+        grad = get_grads(torch.Tensor(random_vectors[i, :]), model)
+        grads.append(np.array(grad))
+
+    # Calculate the average gradient and multiply by the difference between the input and baseline
+    integrated_grads = (inp - baseline) * np.mean(grads, axis=0)
+
+    return integrated_grads.reshape(-1,), np.mean(grads, axis=0)
+
+
+def get_integrated_gradients_rect(model, inp, baseline, steps=20):
+
+    # If baseline is not provided, start with an array of zeros
+    if baseline is None:
+        baseline = np.zeros(len(inp)).astype(np.float32)
+    else:
+        baseline = baseline
+
+    baseline = np.array(baseline).reshape(-1, len(inp))
+    inp = np.array(inp).reshape(-1, len(inp))
+
+    # Compute gradients for each midpoint
+    # delta = 1 / rects
+    # integrated_grads = np.zeros(len(inp))
+    # for i in range(rects):
+    #     midpoint = baseline + delta * (i + 0.5) * (inp - baseline)
+    #     grad = get_grads(torch.Tensor(midpoint), model)
+    #     integrated_grads += delta * (inp - baseline) * grad
+
+    # 1. Interpolation: path between baseline and input
+    path_inputs = [baseline + (i / steps) * (inp - baseline) for i in range(steps + 1)]
+
+    # 2. Compute gradients
+    grads = []
+    for elem in path_inputs:
+        grad = get_grads(torch.Tensor(elem[0]), model)
+        grads.append(np.array(grad))
+
+    # 3. Approximate the integral using the rectangular rule
+    avg_grads = grads[0]
+    for i in range(1, len(grads)):
+        avg_grads += grads[i]
+    avg_grads /= steps
 
     # 4. Calculate integrated gradients and return
-    integrated_grads = ((inp - baseline) * avg_grads.item())[0]
-    return integrated_grads
+    integrated_grads = ((inp - baseline) * avg_grads)[0]
+
+    return integrated_grads, avg_grads
+
---
+++ training.py
@@ -0,0 +1,74 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import torch
+from torch import nn
+from model import OrthogonalAutoEncoder
+from training_utils import TrainingModel, create_grouped_bar_plot
+from gradients import get_integrated_gradients, get_integrated_gradients_mc, get_integrated_gradients_rect
+
+# AI4I (data from https://archive.ics.uci.edu/ml/datasets/AI4I+2020+Predictive+Maintenance+Dataset)
+# only keeping columns: Air_temperature, Process_temperature, Rotational_speed, Torque, Tool_wear, y
+df = pd.read_csv("/Users/dcac/Data/ai4i/ai4i2020_reduced.csv").iloc[:1000, 3:]
+df_train = df[df["y"] == 0].drop(["y"], axis=1)  # clean training data to fit the model
+df_test = df[df["y"] == 1].drop(["y"], axis=1)
+
+# Scaling data
+scaler = StandardScaler()
+x1 = torch.Tensor(pd.DataFrame(scaler.fit_transform(df_train)).values)
+x2 = torch.Tensor(pd.DataFrame(scaler.transform(df_train)).values)
+
+
+# Initiating model
+model = OrthogonalAutoEncoder(encoding_layers=[5, 5], activation_function=nn.ReLU(), dropout=0)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+print(model)
+sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+# Training model (validation loss is lower because of dropout)
+train = TrainingModel(penalty=0.1, learning_rate=0.001, batch_size=20, val_size=0.20, verbose=True)
+train_loader, valid_loader = train.create_datasets(train_data=x1)
+trained_model, losses = train.train_model(model, patience=10, n_epochs=1000)
+
+# Plot losses
+plt.figure(figsize=(10, 10))
+plt.plot(losses["train"], label="training loss")
+plt.plot(losses["val"], label="validation loss")
+plt.legend()
+plt.show()
+
+# Saving trained model
+# torch.save(trained_model, "orthogonal_autoencoder.pt")
+
+# Computing integrated gradients of the encoder network
+base = x1.mean(axis=0)
+example = x2[0]
+int_grads_tr, _ = get_integrated_gradients(model=trained_model, inp=example, baseline=base, steps=20)
+int_grads_mc, _ = get_integrated_gradients_mc(model=trained_model, inp=example, baseline=base, samples=100)
+
+
+def create_grouped_bar_plot(group_labels, **kwargs):
+    bar_width = 0.15
+    num_groups = len(kwargs)
+
+    # Create a bar plot
+    fig, ax = plt.subplots()
+    x = np.arange(len(group_labels))
+    i = 0
+    for key, value in kwargs.items():
+        ax.bar(x + i*bar_width - (num_groups-1)/2*bar_width, value, bar_width, label=key)
+        i += 1
+
+    # Add labels and a legend
+    ax.set_ylabel('IG Values')
+    ax.axhline(y=0, c="k", lw=0.5)
+    ax.set_xticks(x)
+    ax.set_xticklabels(group_labels, rotation=45)
+    ax.legend()
+
+    plt.show()
+
+# Plotting integrated gradients
+create_grouped_bar_plot(df_train.columns, Trapezoidal=int_grads_tr, Monte_Carlo=int_grads_mc)
